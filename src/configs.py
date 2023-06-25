037c622f8a2c121f0e9e128cc66f301bda6037e2
Guillaume Di Fatta <guillaume.difatta@student-cs.fr>
2023-06-25 18:37:09 +0200

    implementation of tkinter gui

---
+++ src/configs.py
@@ -0,0 +1,258 @@
+import torch
+import torchvision
+from pathlib import Path
+from PIL import Image
+from typing import Tuple, Iterator
+
+from labml import tracker, monit, experiment
+from labml.configs import BaseConfigs
+from labml_helpers.device import DeviceConfigs
+from labml_helpers.train_valid import ModeState, hook_model_outputs
+from labml_nn.utils import cycle_dataloader
+
+from src.generator import Generator, MappingNetwork, GeneratorLoss
+from src.discriminator import Discriminator, DiscriminatorLoss
+from src.utils import PathLengthPenalty, GradientPenalty
+
+
+class Dataset(torch.utils.data.Dataset):
+    def __init__(self, path, img_size):
+        super().__init__()
+
+        self.paths = [p for p in Path(path).glob(f'**/*.jpg')]
+
+        self.transform = torchvision.transforms.Compose(
+            [torchvision.transforms.Resize([img_size, img_size]), torchvision.transforms.ToTensor()])
+
+    def __len__(self):
+        return (len(self.paths))
+
+    def __getitem__(self, index):
+        return (self.transform(Image.open(self.paths[index])))
+
+
+class Configs(BaseConfigs):
+    device: torch.device = DeviceConfigs()
+
+    d_latent: int = 512
+    batch_size: int = 16
+    img_size: int = 64
+
+    dataset: Dataset
+    dataset_path: str
+    loader: Iterator
+
+    discriminator: Discriminator
+    generator: Generator
+    mapping_network: MappingNetwork
+
+    discriminator_loss: DiscriminatorLoss
+    generator_loss: GeneratorLoss
+
+    discriminator_optimizer: torch.optim.Adam
+    generator_optimizer: torch.optim.Adam
+    mapping_network_optimizer: torch.optim.Adam
+
+    num_gen_block: int
+
+    learning_rate: float = 1e-3
+    mn_learning_rate: float = 1e-5
+    adam_betas: Tuple[float, float] = (0.0, 0.99)
+    training_steps: int = 90000
+
+    style_mix_prob: float = 0.9
+
+    log_generated_interval: int = 500
+    gradient_accumulate_steps: int = 1
+
+    gradient_penalty: GradientPenalty
+    path_length_penalty: PathLengthPenalty
+
+    lazy_gradient_penalty_interval: int = 4
+    gradient_penalty: GradientPenalty
+    gradient_penalty_coefficient: float = 10.
+
+    lazy_path_penalty_interval: int = 32
+    lazy_path_penalty_after: int = 5000
+
+    save_checkpoint_interval: int = 1000
+
+    log_layer_outputs: bool = False
+    mode = ModeState()
+
+    def init(self, path):
+        self.dataset_path = path
+
+        self.dataset = Dataset(self.dataset_path, self.img_size)
+        dataloader = torch.utils.data.DataLoader(self.dataset, batch_size=self.batch_size, num_workers=8,
+                                                 shuffle=True, drop_last=True, pin_memory=True)
+
+        self.loader = cycle_dataloader(dataloader)
+
+        self.discriminator = Discriminator(
+            self.img_size, n_features=64, max_features=512).to(self.device)
+        self.generator = Generator(
+            self.img_size, self.d_latent, n_features=32, max_features=512).to(self.device)
+
+        self.num_gen_block = self.generator.num_blocks
+
+        self.mapping_network = MappingNetwork(
+            8, self.d_latent).to(self.device)
+
+        self.path_length_penalty = PathLengthPenalty(0.99).to(self.device)
+        self.gradient_penalty = GradientPenalty()
+
+        if (self.log_layer_outputs):
+            hook_model_outputs(self.mode, self.discriminator, 'discriminator')
+            hook_model_outputs(self.mode, self.generator, 'generator')
+            hook_model_outputs(
+                self.mode, self.mapping_network, 'mapping_network')
+
+        self.discriminator_loss = DiscriminatorLoss().to(self.device)
+        self.generator_loss = GeneratorLoss().to(self.device)
+
+        self.discriminator_optimizer = torch.optim.Adam(
+            self.discriminator.parameters(), lr=self.learning_rate, betas=self.adam_betas)
+        self.generator_optimizer = torch.optim.Adam(
+            self.generator.parameters(), lr=self.learning_rate, betas=self.adam_betas)
+        self.mapping_network_optimizer = torch.optim.Adam(
+            self.mapping_network.parameters(), lr=self.mn_learning_rate, betas=self.adam_betas)
+
+        tracker.set_image("generated", True)
+
+    def get_w(self, batch_size):
+        if (torch.rand(()).item() < self.style_mix_prob):
+            cross_over_point = int(torch.rand(()).item() * self.num_gen_block)
+
+            z1 = torch.randn(batch_size, self.d_latent).to(self.device)
+            z2 = torch.randn(batch_size, self.d_latent).to(self.device)
+
+            w1 = self.mapping_network(z1)
+            w2 = self.mapping_network(z2)
+
+            w1 = w1[None, :, :].expand(cross_over_point, -1, -1)
+            w2 = w2[None, :, :].expand(
+                self.num_gen_block-cross_over_point, -1, -1)
+
+            return (torch.cat((w1, w2), dim=0))
+
+        else:
+            z = torch.randn(batch_size, self.d_latent).to(self.device)
+
+            w = self.mapping_network(z)
+
+            return (w[None, :, :].expand(self.num_gen_block, -1, -1))
+
+    def get_noise(self, batch_size):
+        noise = []
+
+        res = 4
+
+        for i in range(self.num_gen_block):
+            if (i == 0):
+                n1 = None
+
+            else:
+                n1 = torch.randn(batch_size, 1, res, res, device=self.device)
+
+            n2 = torch.randn(batch_size, 1, res, res, device=self.device)
+
+            noise.append((n1, n2))
+            res *= 2
+
+        return (noise)
+
+    def generate_images(self, batch_size):
+        w = self.get_w(batch_size)
+        noise = self.get_noise(batch_size)
+
+        img = self.generator(w, noise)
+
+        return (img, w)
+
+    def step(self, index):
+        with (monit.section("Discriminator")):
+            self.discriminator_optimizer.zero_grad()
+
+            for i in range(self.gradient_accumulate_steps):
+                with (self.mode.update(is_log_activations=(index+1) % self.log_generated_interval == 0)):
+                    gen_img, w = self.generate_images(self.batch_size)
+
+                    fake_output = self.discriminator(gen_img.detach())
+
+                    real_img = next(self.loader).to(self.device)
+
+                    if ((index+1) % self.lazy_gradient_penalty_interval == 0):
+                        real_img.requires_grad_()
+
+                    real_output = self.discriminator(real_img)
+
+                    real_loss, fake_loss = self.discriminator_loss(
+                        real_output, fake_output)
+                    disc_loss = real_loss+fake_loss
+
+                    if ((index+1) % self.lazy_gradient_penalty_interval == 0):
+                        gp = self.gradient_penalty(real_img, real_output)
+                        tracker.add('loss.gp', gp)
+
+                        disc_loss = disc_loss + 0.5*self.gradient_penalty_coefficient * \
+                            gp*self.lazy_gradient_penalty_interval
+
+                    disc_loss.backward()
+
+                    tracker.add('loss_discriminator', disc_loss)
+
+            if ((index+1) % self.log_generated_interval == 0):
+                tracker.add('discriminator', self.discriminator)
+
+            torch.nn.utils.clip_grad_norm_(
+                self.discriminator.parameters(), max_norm=1.)
+            self.discriminator_optimizer.step()
+
+        with (monit.section("Generator")):
+            self.generator_optimizer.zero_grad()
+            self.mapping_network_optimizer.zero_grad()
+
+            for i in range(self.gradient_accumulate_steps):
+                gen_img, w = self.generate_images(self.batch_size)
+
+                fake_output = self.discriminator(gen_img)
+                gen_loss = self.generator_loss(fake_output)
+
+                if (index > self.lazy_path_penalty_after and (index+1) % self.lazy_path_penalty_interval == 0):
+                    plp = self.path_length_penalty(w, gen_img)
+
+                    if (not (torch.isnan(plp))):
+                        tracker.add('loss.plp', plp)
+                        gen_loss = gen_loss + plp
+
+                gen_loss.backward()
+                tracker.add('generator_loss', gen_loss)
+
+            if ((index+1) % self.log_generated_interval == 0):
+                tracker.add('generator', self.generator)
+                tracker.add('mapping_network', self.mapping_network)
+
+            torch.nn.utils.clip_grad_norm_(
+                self.generator.parameters(), max_norm=1.)
+            torch.nn.utils.clip_grad_norm_(
+                self.mapping_network.parameters(), max_norm=1.)
+
+            self.generator_optimizer.step()
+            self.mapping_network_optimizer.step()
+
+        if ((index+1) % self.log_generated_interval == 0):
+            tracker.add('generated', torch.cat(
+                [gen_img[:6], real_img[:3]], dim=0))
+
+        if ((index+1) % self.save_checkpoint_interval == 0):
+            experiment.save_checkpoint()
+
+    tracker.save()
+
+    def train(self):
+        for i in monit.loop(self.training_steps):
+            self.step(i)
+
+            if ((i+1) % self.log_generated_interval == 0):
+                tracker.new_line()
---
+++ src/functions.py
@@ -0,0 +1,154 @@
+import torch
+from PIL import Image, ImageTk
+import numpy as np
+import tkinter as tk
+from super_image import ImageLoader, EdsrModel
+from labml import experiment, lab
+import os
+
+from src.configs import Configs
+
+
+def train_new_model():
+    experiment.create(name="waifu-gan")
+
+    configs = Configs()
+
+    experiment.configs(configs, {
+        'device.cuda_device': 0,
+        'img_size': 64,
+        'log_generated_interval': 200
+    })
+
+    configs.init(str(lab.get_data_path() / 'stylegan'))
+
+    experiment.add_pytorch_models(mapping_network=configs.mapping_network,
+                                  generator=configs.generator,
+                                  discriminator=configs.discriminator)
+
+    with (experiment.start()):
+        configs.train()
+
+
+def train_existing_model():
+    experiment.create(name="waifu-gan")
+
+    configs = Configs()
+
+    experiment.configs(configs, {
+        'device.cuda_device': 0,
+        'img_size': 64,
+        'log_generated_interval': 200
+    })
+
+    configs.init(str(lab.get_data_path() / 'stylegan'))
+
+    configs.discriminator.load_state_dict(
+        torch.load("best_model/discriminator.pth"))
+    configs.mapping_network.load_state_dict(
+        torch.load("best_model/mapping_network.pth"))
+    configs.generator.load_state_dict(torch.load("best_model/generator.pth"))
+
+    experiment.add_pytorch_models(mapping_network=configs.mapping_network,
+                                  generator=configs.generator,
+                                  discriminator=configs.discriminator)
+
+    with (experiment.start()):
+        configs.train()
+
+
+def convert_generated_images(img):
+    img = np.transpose(img.detach().cpu().numpy(), (1, 2, 0))
+    img = np.clip(img, 0, 1)
+
+    pil_image = Image.fromarray((img*255).astype("uint8"))
+
+    return (pil_image)
+
+
+def upscale_image(img, scale):
+    model = EdsrModel.from_pretrained('eugenesiow/edsr-base', scale=scale)
+    inputs = ImageLoader.load_image(img)
+    scaled_img = model(inputs)
+
+    pil_scaled_img = convert_generated_images(scaled_img[0])
+
+    return (pil_scaled_img)
+
+
+def generate_new_images(configs):
+
+    img, w = configs.generate_images(configs.batch_size)
+
+    pil_images = list(map(convert_generated_images, img))
+    pil_scaled_images = list(
+        map(lambda img: upscale_image(img, 4), pil_images))
+
+    for i in range(len(pil_scaled_images)):
+        pil_scaled_images[i].save("generated/"+str(i)+".jpg")
+
+
+def load_images(canvas, batch_size):
+    global tk_images
+
+    tk_images = []
+
+    for i in range(batch_size):
+        tk_image = ImageTk.PhotoImage(file="generated/"+str(i)+".jpg")
+        tk_images.append(tk_image)
+
+        row = i//4
+        col = i % 4
+
+        canvas.create_image(col*256+130, row*256+130, image=tk_images[i])
+
+
+def update_images(configs, canvas, nb_img_display):
+    generate_new_images(configs)
+    load_images(canvas, 2**(int(nb_img_display)-1))
+
+
+def change_nb_images(text, canvas, v):
+    nb_img = 2**(int(v)-1)
+
+    text["text"] = str(nb_img)
+    if (nb_img == 16):
+        text["text"] = "12"
+
+    load_images(canvas, nb_img)
+
+
+def init_generation():
+
+    configs = Configs()
+    configs.init(str(lab.get_data_path() / 'stylegan'))
+    configs.mapping_network.load_state_dict(
+        torch.load("best_model/mapping_network.pth"))
+    configs.generator.load_state_dict(torch.load("best_model/generator.pth"))
+
+    root = tk.Tk()
+    root.geometry("1200x850")
+
+    canvas = tk.Canvas(root, width=1024, height=768, bg="white")
+    canvas.pack()
+
+    nb_img_display = tk.DoubleVar(value=1)
+
+    text = tk.Label(root, text=str(int(2**(nb_img_display.get()-1))))
+    scale = tk.Scale(root, variable=nb_img_display, from_=1, to=5,
+                     orient=tk.HORIZONTAL, showvalue=0, command=lambda nb_img_display: change_nb_images(text, canvas, nb_img_display))
+    scale.pack()
+    text.pack()
+
+    boutton = tk.Button(root, text="generate", command=lambda: update_images(
+        configs, canvas, nb_img_display.get()))
+    boutton.pack()
+
+    update_images(configs, canvas, nb_img_display.get())
+
+    def on_closing():
+        root.quit()
+    root.protocol("WM_DELETE_WINDOW", on_closing)
+    root.mainloop()
+
+    return (root, canvas)
